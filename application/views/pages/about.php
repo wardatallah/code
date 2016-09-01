1649f6ab86e3dede23de812cea709d1d0992d403
belalalsaid <belal.alsaid.123@gmail.com>
2016-09-01 14:37:47 +0200

    create about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,256 +1,105 @@
 <div class="content"> 
     
     <!--======= SUB BANNER =========-->
-    <section class="sub-banner animate fadeInUp" data-wow-delay="0.4s">
-      <div class="container">
-        <h4>ABOUT US</h4>
-        <!-- Breadcrumb -->
-        <ol class="breadcrumb">
-          <li><a href="#">Home</a></li>
-          <li class="active">ABOUT US</li>
-        </ol>
-      </div>
-    </section>
-    
-    <!--======= ABOUT IMG LARGE =========-->
-    <div class="row">
-      <div class="col-md-6 animate fadeInLeft" data-wow-delay="0.4s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/about-img-1.jpg" alt=""> </div>
-      <div class="col-md-6 animate fadeInRight" data-wow-delay="0.4s"> 
-        <!--======= About Us =========-->
-        <section class="section-p-30px about-us about-con">
-          <div class="sma-hed">
-            <h5>THE MOST INFLUENTIAL PERSONAL STYLE BLOGGERS RIGHT NOW</h5>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal as opposed to using 'Content here, </p>
-            
-            <hr>
-          </div>
-          <div class="about-detail">
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.<br>
-            </p>
-          </div>
-        </section>
-      </div>
-    </div>
-    
-    <!--======= Services =========-->
-    <section class="section-p-60px about-us">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-5 animate fadeInLeft" data-wow-delay="0.4s">
-            <div class="sma-hed">
-              <h5>WHY WE ARE THE BEST ?</h5>
-            </div>
-            <div class="about-detail">
-              <p>omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates <br>
-                <br>
-                repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
-              
-              <!--  About Featured -->
-              <ul class="about-feat">
-                
-                <!--  WORLD WIDE SHIP -->
-                <li class="animate fadeInUp" data-wow-delay="0.4s">
-                  <div class="media">
-                    <div class="media-left"> <i class="fa fa-paper-plane-o"></i> </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">WORLD WIDE SHIP</h6>
-                      <p>All oder over $85.00</p>
-                    </div>
-                  </div>
-                </li>
-                
-                <!--  MONEY BACK -->
-                <li class="animate fadeInUp" data-wow-delay="0.4s">
-                  <div class="media">
-                    <div class="media-left"> <i class="fa fa-usd"></i> </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">MONEY BACK</h6>
-                      <p>100% Money Back - 30days</p>
-                    </div>
-                  </div>
-                </li>
-                
-                <!--  BEST SUPPORT -->
-                <li class="animate fadeInUp" data-wow-delay="0.4s">
-                  <div class="media">
-                    <div class="media-left"> <i class="fa fa-whatsapp"></i> </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">BEST SUPPORT</h6>
-                      <p>Fast & pro 24/7</p>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-          
-          <!--  Video Section -->
-          <div class="col-md-7 animate fadeInRight" data-wow-delay="0.4s"> <img src="<?php echo base_url().'assets/'; ?>images/about-img-3.jpg" alt=""> 
-            
-            <!--  Video Section -->
-            <div class="position-center-center"> <a href="#pop-open" class="link popup-vedio video-btn"></a> </div>
-            
-            <!--======= POPUP VIDEO START =========-->
-            <div id="pop-open" class="zoom-anim-dialog mfp-hide pop-open-style">
-              <div class="pop_up">
-                <div class="video"> 
-                  <!--  Video Link -->
-                  <iframe src="https://player.vimeo.com/video/7449107"></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    
-    <!--======= TEAM =========-->
-    <section class="section-p-60px team team-over"> 
-      
-      <!-- TITTLE -->
-      <div class="tittle animate fadeInUp" data-wow-delay="0.4s">
-        <h5>OUR TEAM</h5>
-        <p>Crazy team </p>
-      </div>
-      <ul>
-        
-        <!-- TEAM 1 -->
-        <li class="animate fadeInUp" data-wow-delay="0.2s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-2-1.jpg" alt="" > 
-          <!-- TEAM HOVE -->
-          <div class="over-team">
-            <h4>JOHN DOE</h4>
-            <h6>CEO/Cofounder</h6>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati</p>
-            
-            <!--======= FOOTER ICONS =========-->
-            <ul class="social_icons">
-              <li class="facebook"><a href="#."> <i class="fa fa-facebook"></i></a></li>
-              <li class="twitter"><a href="#."> <i class="fa fa-twitter"></i></a></li>
-              <li class="behance"><a href="#."> <i class="fa fa-behance"></i></a></li>
-              <li class="skype"><a href="#."> <i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-        </li>
-        
-        <!-- TEAM 2 -->
-        <li class="animate fadeInUp" data-wow-delay="0.4s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-2-2.jpg" alt="" > 
-          <!-- TEAM HOVE -->
-          <div class="over-team">
-            <h4>MARRY DOE</h4>
-            <h6>CEO/Cofounder</h6>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati</p>
-            
-            <!--======= FOOTER ICONS =========-->
-            <ul class="social_icons">
-              <li class="facebook"><a href="#."> <i class="fa fa-facebook"></i></a></li>
-              <li class="twitter"><a href="#."> <i class="fa fa-twitter"></i></a></li>
-              <li class="behance"><a href="#."> <i class="fa fa-behance"></i></a></li>
-              <li class="skype"><a href="#."> <i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-        </li>
-        
-        <!-- TEAM 3 -->
-        <li class="animate fadeInUp" data-wow-delay="0.6s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-2-3.jpg" alt="" > 
-          <!-- TEAM HOVE -->
-          <div class="over-team">
-            <h4>LADY TECH</h4>
-            <h6>CEO/Cofounder</h6>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati</p>
-            
-            <!--======= FOOTER ICONS =========-->
-            <ul class="social_icons">
-              <li class="facebook"><a href="#."> <i class="fa fa-facebook"></i></a></li>
-              <li class="twitter"><a href="#."> <i class="fa fa-twitter"></i></a></li>
-              <li class="behance"><a href="#."> <i class="fa fa-behance"></i></a></li>
-              <li class="skype"><a href="#."> <i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-        </li>
-        
-        <!-- TEAM 4 -->
-        <li class="animate fadeInUp" data-wow-delay="0.8s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-2-4.jpg" alt="" > 
-          <!-- TEAM HOVE -->
-          <div class="over-team">
-            <h4>JOHN DOE</h4>
-            <h6>CEO/Cofounder</h6>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati</p>
-            
-            <!--======= FOOTER ICONS =========-->
-            <ul class="social_icons">
-              <li class="facebook"><a href="#."> <i class="fa fa-facebook"></i></a></li>
-              <li class="twitter"><a href="#."> <i class="fa fa-twitter"></i></a></li>
-              <li class="behance"><a href="#."> <i class="fa fa-behance"></i></a></li>
-              <li class="skype"><a href="#."> <i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-        </li>
-        
-        <!-- TEAM 5 -->
-        <li class="animate fadeInUp" data-wow-delay="1s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-2-5.jpg" alt="" > 
-          <!-- TEAM HOVE -->
-          <div class="over-team">
-            <h4>MARRY DOE</h4>
-            <h6>CEO/Cofounder</h6>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati</p>
-            
-            <!--======= FOOTER ICONS =========-->
-            <ul class="social_icons">
-              <li class="facebook"><a href="#."> <i class="fa fa-facebook"></i></a></li>
-              <li class="twitter"><a href="#."> <i class="fa fa-twitter"></i></a></li>
-              <li class="behance"><a href="#."> <i class="fa fa-behance"></i></a></li>
-              <li class="skype"><a href="#."> <i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-    </section>
-    
-    <!--======= Services =========-->
-    <section class="section-p-60px no-padding-t services">
+	<section class="section-p-60px services welcome no-padding-b">
       <div class="container"> 
         <!--  Tittle -->
-        <div class="tittle animate fadeInUp" data-wow-delay="0.4s">
-          <h5>OUR SERVICES</h5>
-          <p>Creat Our Pervfect  Online Shop</p>
+         <div class="tittle tittle-cont  animate fadeInUp" data-wow-delay="0.4s">
+          <h3>WELCOME TO INTERMED CERAMICA</h3>
+          <hr>
+          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised </p>
         </div>
         
         <!--  Services -->
+		<div class="tittle   tittle-cont  animate fadeInUp" data-wow-delay="0.4s">
+          <h5>WHAT WE DO</h5>
+          <hr>
+        </div>
+
         <ul class="row">
-          
-          <!--  Fashion Design -->
-          <li class="col-sm-3 animate fadeInLeft" data-wow-delay="0.4s">
-            <div class="icon"><i class="fa fa-anchor"></i></div>
-            <h6>Fashion Design</h6>
-            <hr>
-            <p class="font-montserrat">Gumbo beet greens corn soko endive gumbo gourd.</p>
+          <!--  WORLD WIDE SHIP -->
+          <li class="col-sm-4  animate fadeInLeft" data-wow-delay="0.4s">
+            <div class="icon"><i class="fa fa-clock-o"></i></div>
+            <h5>WORLD WIDE SHIP</h5>
+            <p>All oder over $85.00</p>
           </li>
           
-          <!--  Fashion Design -->
-          <li class="col-sm-3 animate fadeInLeft" data-wow-delay="0.6s">
-            <div class="icon"><i class="fa fa-headphones"></i></div>
-            <h6>MUSIC PRODUCTION</h6>
-            <hr>
-            <p class="font-montserrat">Gumbo beet greens corn soko endive gumbo gourd.</p>
+          <!--  MONEY BACK -->
+          <li class="col-sm-4  animate fadeInUp" data-wow-delay="0.6s">
+            <div class="icon"><i class="fa fa-dollar"></i></div>
+            <h5>MONEY BACK</h5>
+            <p>100% Money back - 30 Days</p>
           </li>
           
-          <!--  Fashion Design -->
-          <li class="col-sm-3 animate fadeInRight" data-wow-delay="0.4s">
-            <div class="icon"><i class="fa fa-paint-brush"></i></div>
-            <h6>CONCEPT DESIGN</h6>
-            <hr>
-            <p class="font-montserrat">Gumbo beet greens corn soko endive gumbo gourd.</p>
-          </li>
-          
-          <!--  Fashion Design -->
-          <li class="col-sm-3 animate fadeInRight" data-wow-delay="0.6s">
-            <div class="icon"><i class="fa fa-leaf"></i></div>
-            <h6>WEB DEVELOPMENT</h6>
-            <hr>
-            <p class="font-montserrat">Gumbo beet greens corn soko endive gumbo gourd.</p>
+          <!--  BEST SUPPORT -->
+          <li class="col-sm-4  animate fadeInRight" data-wow-delay="0.8s">
+            <div class="icon"><i class="fa fa-whatsapp"></i></div>
+            <h5>BEST SUPPORT</h5>
+            <p>Fast & pro 24/7</p>
           </li>
         </ul>
       </div>
     </section>
+    
+    <section class="parallex parallex-fur animate fadeInUp" data-wow-delay="0.4s" data-stellar-background-ratio="0.5">
+      <div class="overlay banner">
+        <div class="container">
+          <div class="col-sm-8"><h3 class="text-left" >Get a Quote .... </h3></div>
+          <div class="col-sm-4"><a href="#." class="btn margin-top-20">NOW</a> </div>
+		</div>
+      </div>
+    </section>
+	<section class="section-p-60px team">
+      <div class="container"> 
+        
+        <!-- TITTLE -->
+        <div class="tittle tittle-cont  animate fadeInUp" data-wow-delay="0.4s">
+          <h3>OUR Location</h3>
+          <hr>
+        </div>
+        <ul class="row">
+          
+          <!-- TEAM 1 -->
+          <li class="col-md-4  animate fadeInLeft" data-wow-delay="0.4s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-1.jpg" alt="" >
+            <h4>Location 1</h4>
+          </li>
+          
+          <!-- TEAM 2 -->
+          <li class="col-md-4  animate fadeInUp" data-wow-delay="0.4s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-2.jpg" alt="" >
+            <h4>Location 2</h4>
+          </li>
+          
+          <!-- TEAM 3 -->
+          <li class="col-md-4  animate fadeInRight" data-wow-delay="0.4s"> <img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/team-3.jpg" alt="" >
+            <h4>Location 3</h4>
+          </li>
+        </ul>
+      </div>
+    </section>
+    
+	<section class="section-p-60px our-clients">
+		<div class="container"> 
+		  <!--  Tittle -->
+		  <div class="tittle tittle-cont  animate fadeInUp" data-wow-delay="0.4s">
+			<h3>OUR CLIENTS</h3>
+			<hr>
+			
+			<!--  Text Intro -->
+			<p class="font-montserrat intro text-center">Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens danadelion.</p>
+		  </div>
+		  
+		  <!--  Client Logo Slider -->
+		  <div class="client-slide  animate fadeInUp" data-wow-delay="0.4s">
+			<div class="slide"><a href="#."><img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/client-logo-1.png" alt=""></a></div>
+			<div class="slide"><a href="#."><img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/client-logo-2.png" alt=""></a></div>
+			<div class="slide"><a href="#."><img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/client-logo-3.png" alt=""></a></div>
+			<div class="slide"><a href="#."><img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/client-logo-4.png" alt=""></a></div>
+			<div class="slide"><a href="#."><img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/client-logo-2.png" alt=""></a></div>
+			<div class="slide"><a href="#."><img class="img-responsive" src="<?php echo base_url().'assets/'; ?>images/client-logo-4.png" alt=""></a></div>
+		  </div>
+		</div>
+    </section>
+  
+     
   </div>
   
